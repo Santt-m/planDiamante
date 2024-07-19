11486336205b2520b189817f690eaa762331ee39
Santiago Maruottolo <smbsanti@gmail.com>
2024-07-18 23:51:30 -0300

    vamos arriba

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('MongoDB Connected...');
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
-const spaRoutes = require('./routes/spaRoutes');
-const therapistRoutes = require('./routes/therapistRoutes');
-const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
 
+// Configurar variables de entorno
 dotenv.config();
-
-connectDB();
 
 const app = express();
 
@@ -22,9 +17,56 @@ app.use(cors());
 
 app.use(express.json());
 
+// Definir rutas
+const spaRoutes = require('./routes/spaRoutes');
+const therapistRoutes = require('./routes/therapistRoutes');
+const authRoutes = require('./routes/authRoutes');
+
 app.use('/api/spas', spaRoutes);
 app.use('/api/therapists', therapistRoutes);
 app.use('/api/auth', authRoutes);
+
+// Función para obtener la URI de MongoDB según el tipo de acceso
+const getMongoURI = (accessType) => {
+  let publicKey, privateKey;
+
+  switch (accessType) {
+    case 'readonly':
+      publicKey = process.env.MONGO_READONLY_PUBLIC_KEY;
+      privateKey = process.env.MONGO_READONLY_PRIVATE_KEY;
+      break;
+    case 'readwrite':
+      publicKey = process.env.MONGO_READWRITE_PUBLIC_KEY;
+      privateKey = process.env.MONGO_READWRITE_PRIVATE_KEY;
+      break;
+    case 'admin':
+      publicKey = process.env.MONGO_ADMIN_PUBLIC_KEY;
+      privateKey = process.env.MONGO_ADMIN_PRIVATE_KEY;
+      break;
+    default:
+      throw new Error('Invalid access type');
+  }
+
+  return `mongodb+srv://${publicKey}:${privateKey}@cluster0.81svvx6.mongodb.net/planDiamanteDB?retryWrites=true&w=majority`;
+};
+
+// Conectar a la base de datos con nivel de acceso de lectura por defecto
+const connectDB = async (accessType = 'readonly') => {
+  const mongoose = require('mongoose');
+  try {
+    const mongoURI = getMongoURI(accessType);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB(); // Puedes cambiar 'readonly' a 'readwrite' o 'admin' según sea necesario
 
 const PORT = process.env.PORT || 5000;
 
